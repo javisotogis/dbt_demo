2d9d0eb30f3204a211567c08be9fd1746218c9e6
Antonio Javier Soto Molina <anjavi.somo@gmail.com>
2026-09-03 14:26:33 +0000

    dims and fact table created

---
+++ macros/date_utils.sql
@@ -0,0 +1,16 @@
+{% macro date_function(x) %}
+
+CASE
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (12, 1, 2)
+        THEN 'WINTER'
+
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (3, 4, 5)
+        THEN 'SPRING'
+
+    WHEN MONTH(TO_TIMESTAMP({{ x }})) IN (6, 7, 8)
+        THEN 'SUMMER'
+
+    ELSE 'AUTUMN'
+END AS STATION_OF_YEAR
+
+{% endmacro %}
